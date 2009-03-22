fbbd1f472b31f4fb1386e1059e212f1eaabc01f3
Justin Azoff <justin@bouncybouncy.net>
2009-03-22 16:44:11 -0400

    nicer setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,32 @@ from glob import glob
 
 setup(name="cymruwhois",
     version="1.0",
+    description="Client for the whois.cymru.com service",
+    long_description="""
+Perform lookups by ip address and return ASN, Country Code, and Netblock Owner::
+
+    >>> import socket
+    >>> ip = socket.gethostbyname("www.google.com")
+    >>> from cymruwhois import Client
+    >>> c=Client()
+    >>> r=c.lookup(ip)
+    >>> print r.asn
+    15169
+    >>> print r.owner
+    GOOGLE - Google Inc.
+
+    """,
+
+    url="http://github.com/JustinAzoff/python-cymruwhois/tree/master",
+    classifiers=[
+        "Topic :: System :: Networking",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    keywords='ASN',
     author="Justin Azoff",
     author_email="JAzoff@uamail.albany.edu",
     py_modules = ["cymruwhois"], 
